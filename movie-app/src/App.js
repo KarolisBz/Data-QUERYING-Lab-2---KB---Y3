245a8b6b1250c11fbe47c4c91f84ad133e2782a4
Karolis Bandziulis <g00417529@atu.ie>
2024-10-04 13:27:36 +0000

    Initial commit

---
+++ movie-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let x = 5;
+
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+}
+
+export default App;
