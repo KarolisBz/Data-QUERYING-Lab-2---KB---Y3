706f53478093548791ce685f3cf60d0bd47fe57a
Karolis Bandziulis <g00417529@atu.ie>
2024-10-04 14:24:34 +0000

    Ex5: Complete + commented

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,13 +1,18 @@
+// importing router for navigation capabilities
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// importing components
 import NavigationBar from './components/NavigationBar';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Content from './components/Content';
 
+// Added Navbar which uses Reacts ROUTER LINK to navigate to different
+// pages which we have set as components and js xml tags
 function App() {
   return (
     <Router>
       <NavigationBar />
+      <Header />
       <Routes>
         <Route path="/home" element={<Content />} />
         <Route path="/read" element={<h1>Read Component</h1>} />
